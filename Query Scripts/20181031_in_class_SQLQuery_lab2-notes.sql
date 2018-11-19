4e40888bab57309646682815d443b2722d53f925
trevor.cullingsworth <trevor.cullingsworth@my.denver.coloradotech.edu>
2018-11-19 15:44:18 -0700

    Updating repo for course assignment work

--- Query Scripts/20181031_in_class_SQLQuery_lab2-notes.sql
+++ Query Scripts/20181031_in_class_SQLQuery_lab2-notes.sql
@@ -5,9 +5,9 @@
 			   top(1)
 			   l.Name as LanguageName,
 			   sum(l.Percentage *   C.Population ) as NumerOfSpeakers
-		from Encompasses e
-		join Country c on e.Country = c.Code and e.Continent='Europe'
-		join [Language] l on l.Country = c.Code
+		from dbo.Encompasses e
+		join dbo.Country c on e.Country = c.Code and e.Continent='Europe'
+		join dbo.[Language] l on l.Country = c.Code
 		group by l.Name
 		order by NumerOfSpeakers desc
 
@@ -17,3 +17,24 @@
 --	then find language spoken in each country
 --  do the calculation with percentage 
 --  and sum up the population from the calculation
+
+
+SELECT C.[Name],
+	   E.[Continent]
+FROM dbo.encompasses E
+JOIN dbo.Country C ON E.[Country] = C.[Code]
+WHERE E.[Continent] = 'Europe'
+ORDER BY C.[Name] ASC;
+
+
+
+SELECT DISTINCT C.[Name] AS 'Country Name',
+	   L1.[Lake] AS 'Lake Name',
+	   SUM(L2.[Area]) AS 'Lake Area'
+FROM dbo.geo_Lake L1
+JOIN dbo.Country C ON C.[Code] = L1.[Country]
+JOIN dbo.Lake L2 ON L2.[Name] = L1.[Lake]
+GROUP BY C.[Name],
+	     'Lake Name'
+ORDER BY 'Lake Area' DESC;
+
